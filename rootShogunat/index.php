febf7dcad95748c6efcf8f146a7a1ddf7ef15b0b
Vaaaz <seb54001@hotmail.fr>
2017-05-03 22:27:15 +0200

    Removed 2 useless files from PhPTutorial

---
+++ rootShogunat/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Learning PhP</title>
+  <link rel="stylesheet" href="DatStyle.css">
+</head>
+<body>
+    <img src="Images/Vaaaz_Logo.png" class="logo"/>
+    <div class="WebDesign"> <img src="Images/WebDesign.png" class="image"/> <div class="Overlay"> </div> </div>
+    <div class="Infographie"> <img src="Images/Infographie.png" class="image" /> <div class="Overlay2"> </div> </div>
+</body>
+</html>
